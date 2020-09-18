395617bd5f7df2c06ad45b37281205d1dc7f2cbf
jRimbault <jacques.rimbault@gmail.com>
2020-09-18 16:10:26 +0200

    walker iterator function

--- src/fs/mod.rs
+++ src/fs/mod.rs
@@ -25,35 +25,19 @@ pub(crate) fn find_dupes_partial<H>(
 where
     H: Hasher + Default,
 {
-    let (sender, receiver) = mpsc::channel();
     let (min, max) = min_max_file_size.unwrap_or((0, u64::MAX));
-    ignore::WalkBuilder::new(dir)
-        .standard_filters(false)
-        .build_parallel()
-        // move sender into closure
-        .run(move || {
-            let sender = sender.clone();
-            Box::new(move |result| {
-                if result.is_err() {
-                    return ignore::WalkState::Continue;
-                }
-                let entry = result.unwrap();
-                let meta = fs::symlink_metadata(entry.path());
-                if meta.is_err() {
-                    return ignore::WalkState::Continue;
-                }
-                let meta = meta.unwrap();
-                let len = meta.len();
-                if meta.is_file() && len >= min && len <= max {
-                    let hasher: FsHasher<H> = Default::default();
-                    if let Ok(hash) = hasher.partial(entry.path()) {
-                        sender.send((hash, DirEntry(entry))).unwrap();
-                    }
-                }
-                ignore::WalkState::Continue
-            })
-        });
-    receiver.into_iter().collect()
+    walker(&dir, |result| {
+        let entry = result.map_err(|_| ())?;
+        let meta = fs::symlink_metadata(entry.path()).map_err(|_| ())?;
+        let len = meta.len();
+        if meta.is_file() && len >= min && len <= max {
+            let hasher: FsHasher<H> = Default::default();
+            if let Ok(hash) = hasher.partial(entry.path()) {
+                return Ok((hash, DirEntry(entry)));
+            }
+        }
+        Err(())
+    })
 }
 
 pub(crate) fn dedupe<H>(counter: TreeBag<u64, DirEntry>) -> TreeBag<u64, DirEntry>
@@ -83,4 +67,26 @@ where
             }
         });
     receiver.into_iter().collect()
+}
+
+fn walker<P: AsRef<Path>, A, I, E, B>(path: &P, walker_fn: A) -> B
+where
+    A: Fn(Result<ignore::DirEntry, ignore::Error>) -> Result<I, E>,
+    A: Send + Copy,
+    B: std::iter::FromIterator<I>,
+    I: Send,
+    E: Send,
+{
+    let (sender, receiver) = mpsc::channel();
+    ignore::WalkBuilder::new(&path)
+        .standard_filters(false)
+        .build_parallel()
+        .run(move || {
+            let sender = sender.clone();
+            Box::new(move |result| {
+                sender.send(walker_fn(result)).unwrap();
+                ignore::WalkState::Continue
+            })
+        });
+    receiver.iter().filter_map(Result::ok).collect()
 }
