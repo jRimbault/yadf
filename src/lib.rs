644feb95442eb8a41efc665d9754c83ff334c884
jRimbault <jacques.rimbault@gmail.com>
2020-09-21 13:10:18 +0200

    update doc test

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,9 @@
 //! Find, display, and report, all the duplicate files at the given path :
 //!
 //! ```ignore
-//! let files_counter = yadf::find_dupes::<twox_hash::XxHash64>(".".as_ref(), None);
-//! print!("{}", files_counter.display::<yadf::Fdupes>());
+//! let paths = vec![PathBuf::from(".")];
+//! let files_counter = yadf::find_dupes::<twox_hash::XxHash64, PathBuf>(&paths, None, None);
+//! println!("{}", files_counter.display::<yadf::Fdupes>());
 //! eprintln!("{}", yadf::Report::from(&files_counter));
 //! ```
 
