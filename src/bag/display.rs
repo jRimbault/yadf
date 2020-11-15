2c6122566a50ded327dc31c27f5ca1a0d6323cb7
jRimbault <jacques.rimbault@gmail.com>
2020-11-15 18:01:49 +0100

    add csv output to library

--- src/bag/display.rs
+++ src/bag/display.rs
@@ -1,5 +1,36 @@
-use super::{Display, Fdupes, Machine};
+use super::{Csv, Display, Fdupes, Machine};
 use std::fmt;
+
+impl<H: Ord, T> fmt::Display for Display<'_, H, T, Csv>
+where
+    T: serde::Serialize,
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        #[derive(serde::Serialize)]
+        struct CsvLine<'a, T: serde::Serialize> {
+            count: usize,
+            bucket: &'a [T],
+        }
+
+        let mut writer = csv::WriterBuilder::new()
+            .flexible(true)
+            .has_headers(false)
+            .from_writer(FmtIoWriter(f));
+
+        writer
+            .write_record(&["count", "bucket"])
+            .map_err(|_| fmt::Error)?;
+        for bucket in self.duplicates.iter() {
+            writer
+                .serialize(CsvLine {
+                    count: bucket.len(),
+                    bucket,
+                })
+                .map_err(|_| fmt::Error)?;
+        }
+        Ok(())
+    }
+}
 
 impl<H: Ord, T> fmt::Display for Display<'_, H, T, Fdupes>
 where
@@ -45,6 +76,22 @@ where
     }
 }
 
+struct FmtIoWriter<'a, 'b>(&'a mut fmt::Formatter<'b>);
+
+impl<'a, 'b> std::io::Write for FmtIoWriter<'a, 'b> {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        let str_ref = std::str::from_utf8(buf)
+            .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+        self.0
+            .write_str(str_ref)
+            .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+        Ok(buf.len())
+    }
+    fn flush(&mut self) -> std::io::Result<()> {
+        Ok(())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::super::TreeBag;
@@ -80,6 +127,26 @@ mod tests {
         .collect();
         let result = counter.duplicates().display::<Fdupes>().to_string();
         let expected = "foo\nbar\n\nhello\nworld";
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn csv() {
+        let counter: TreeBag<i32, &str> = vec![
+            (77, "hello"),
+            (77, "world"),
+            (1, "ignored"),
+            (3, "foo"),
+            (3, "bar"),
+        ]
+        .into_iter()
+        .collect();
+        let result = counter.duplicates().display::<Csv>().to_string();
+        let expected = "\
+            count,bucket\n\
+            2,foo,bar\n\
+            2,hello,world\n\
+        ";
         assert_eq!(result, expected);
     }
 }
