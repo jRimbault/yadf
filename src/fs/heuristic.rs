59385a0708e80886e14b96b93db23dac7ab4d7b0
jRimbault <jacques.rimbault@gmail.com>
2020-12-02 16:39:49 +0100

    cleanup module

--- src/fs/heuristic.rs
+++ src/fs/heuristic.rs
@@ -2,12 +2,23 @@ use once_cell::sync::Lazy;
 use std::path::{Path, PathBuf};
 use sysinfo::{DiskExt, DiskType, SystemExt};
 
-#[derive(Debug)]
-struct System {
-    num_cpu: usize,
-    #[cfg(not(windows))]
-    root: Disk,
-    mounted: Vec<Disk>,
+#[cfg(not(windows))]
+use unix::disk_type;
+#[cfg(windows)]
+use win::disk_type;
+
+pub fn num_cpus_get<P: AsRef<Path>>(paths: &[P]) -> usize {
+    let (ssds, others): (Vec<DiskType>, Vec<DiskType>) = paths
+        .iter()
+        .map(disk_type)
+        .partition(|&disk_type| disk_type == DiskType::SSD);
+    // study a better heuristics here,
+    // unfortunately I don't have any internal HDDs to test things with
+    if ssds.len() > others.len() {
+        num_cpus::get()
+    } else {
+        num_cpus::get() / 2
+    }
 }
 
 #[derive(Debug)]
@@ -22,57 +33,6 @@ impl From<(PathBuf, DiskType)> for Disk {
     }
 }
 
-#[cfg(windows)]
-static SYSTEM: Lazy<System> = Lazy::new(|| System {
-    num_cpu: num_cpus::get(),
-    mounted: sysinfo::System::new_all()
-        .get_disks()
-        .iter()
-        .map(|disk| (disk.get_mount_point().to_owned(), disk.get_type()))
-        .map(Into::into)
-        .collect(),
-});
-
-#[cfg(not(windows))]
-static SYSTEM: Lazy<System> = Lazy::new(|| {
-    use std::path::Component;
-    let infos = sysinfo::System::new_all();
-    let mut system = System {
-        num_cpu: num_cpus::get(),
-        root: ("".into(), DiskType::Unknown(-1)).into(),
-        mounted: Vec::new(),
-    };
-    for disk in infos.get_disks() {
-        let mut components = disk.get_mount_point().components();
-        if let (Some(Component::RootDir), None) = (components.next(), components.next()) {
-            system.root = (disk.get_mount_point().to_owned(), disk.get_type()).into();
-            continue;
-        }
-        system
-            .mounted
-            .push((disk.get_mount_point().to_owned(), disk.get_type()).into());
-    }
-    system
-});
-
-#[cfg(windows)]
-fn disk_type<P: AsRef<Path>>(path: &P) -> DiskType {
-    let path = dunce::canonicalize(path.as_ref()).unwrap();
-    find_disk(&path, &SYSTEM.mounted).unwrap_or(DiskType::Unknown(-1))
-}
-
-#[cfg(not(windows))]
-fn disk_type<P: AsRef<Path>>(path: &P) -> DiskType {
-    let path = dunce::canonicalize(path.as_ref()).unwrap();
-    find_disk(&path, &SYSTEM.mounted).unwrap_or_else(|| {
-        if path.starts_with(&SYSTEM.root.path) {
-            SYSTEM.root.disk_type
-        } else {
-            DiskType::Unknown(-1)
-        }
-    })
-}
-
 fn find_disk(path: &Path, disks: &[Disk]) -> Option<DiskType> {
     disks
         .iter()
@@ -80,17 +40,69 @@ fn find_disk(path: &Path, disks: &[Disk]) -> Option<DiskType> {
         .map(|disk| disk.disk_type)
 }
 
-pub fn num_cpus_get<P: AsRef<Path>>(paths: &[P]) -> usize {
-    let (ssds, others): (Vec<DiskType>, Vec<DiskType>) = paths
-        .iter()
-        .map(disk_type)
-        .partition(|&disk_type| disk_type == DiskType::SSD);
-    // study a better heuristics here,
-    // unfortunately I don't have any internal HDDs to test things with
-    if ssds.len() > others.len() {
-        SYSTEM.num_cpu
-    } else {
-        SYSTEM.num_cpu / 2
+#[allow(dead_code)]
+mod win {
+    use super::*;
+
+    #[derive(Debug)]
+    struct System {
+        mounted: Vec<Disk>,
+    }
+
+    static SYSTEM: Lazy<System> = Lazy::new(|| System {
+        mounted: sysinfo::System::new_all()
+            .get_disks()
+            .iter()
+            .map(|disk| (disk.get_mount_point().to_owned(), disk.get_type()))
+            .map(Into::into)
+            .collect(),
+    });
+
+    pub fn disk_type<P: AsRef<Path>>(path: &P) -> DiskType {
+        let path = dunce::canonicalize(path.as_ref()).unwrap();
+        find_disk(&path, &SYSTEM.mounted).unwrap_or(DiskType::Unknown(-1))
+    }
+}
+
+#[allow(dead_code)]
+mod unix {
+    use super::*;
+
+    #[derive(Debug)]
+    struct System {
+        root: Disk,
+        mounted: Vec<Disk>,
+    }
+
+    static SYSTEM: Lazy<System> = Lazy::new(|| {
+        use std::path::Component;
+        let infos = sysinfo::System::new_all();
+        let mut system = System {
+            root: ("".into(), DiskType::Unknown(-1)).into(),
+            mounted: Vec::new(),
+        };
+        for disk in infos.get_disks() {
+            let mut components = disk.get_mount_point().components();
+            if let (Some(Component::RootDir), None) = (components.next(), components.next()) {
+                system.root = (disk.get_mount_point().to_owned(), disk.get_type()).into();
+                continue;
+            }
+            system
+                .mounted
+                .push((disk.get_mount_point().to_owned(), disk.get_type()).into());
+        }
+        system
+    });
+
+    pub fn disk_type<P: AsRef<Path>>(path: &P) -> DiskType {
+        let path = dunce::canonicalize(path.as_ref()).unwrap();
+        find_disk(&path, &SYSTEM.mounted).unwrap_or_else(|| {
+            if path.starts_with(&SYSTEM.root.path) {
+                SYSTEM.root.disk_type
+            } else {
+                DiskType::Unknown(-1)
+            }
+        })
     }
 }
 
