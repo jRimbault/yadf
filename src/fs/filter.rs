7d7c7f51361622929fb32b54f7eab0ddacd3d103
jRimbault <jacques.rimbault@gmail.com>
2021-02-21 18:51:00 +0100

    refactor: cleanup the filters module

--- src/fs/filter.rs
+++ src/fs/filter.rs
@@ -1,9 +1,6 @@
 use std::fs::Metadata;
 use std::path::Path;
 
-#[cfg(unix)]
-use std::{collections::HashSet, os::unix::fs::MetadataExt, sync::Mutex};
-
 #[derive(Debug)]
 pub(crate) struct FileFilter {
     min: Option<u64>,
@@ -11,34 +8,39 @@ pub(crate) struct FileFilter {
     regex: Option<regex::Regex>,
     glob: Option<globset::GlobMatcher>,
     #[cfg(unix)]
-    inodes_filter: InodeFilter,
-}
-
-#[derive(Debug)]
-#[cfg(unix)]
-struct InodeFilter {
-    enabled: bool,
-    seen: Mutex<HashSet<u64>>,
+    inodes_filter: inode::Filter,
 }
 
 impl FileFilter {
+    #[cfg(not(unix))]
     pub fn new(
         min: Option<u64>,
         max: Option<u64>,
         regex: Option<regex::Regex>,
         glob: Option<globset::GlobMatcher>,
-        #[cfg(unix)] disable_hard_links_filter: bool,
     ) -> Self {
         Self {
             min,
             max,
             regex,
             glob,
-            #[cfg(unix)]
-            inodes_filter: InodeFilter {
-                enabled: !disable_hard_links_filter,
-                seen: Default::default(),
-            },
+        }
+    }
+
+    #[cfg(unix)]
+    pub fn new(
+        min: Option<u64>,
+        max: Option<u64>,
+        regex: Option<regex::Regex>,
+        glob: Option<globset::GlobMatcher>,
+        disable_hard_links_filter: bool,
+    ) -> Self {
+        Self {
+            min,
+            max,
+            regex,
+            glob,
+            inodes_filter: inode::Filter::new(disable_hard_links_filter),
         }
     }
 
@@ -54,16 +56,6 @@ impl FileFilter {
             && self.max.map_or(true, |m| meta.len() <= m)
             && is_match(&self.regex, path).unwrap_or(true)
             && is_match(&self.glob, path).unwrap_or(true)
-    }
-}
-
-#[cfg(unix)]
-impl InodeFilter {
-    fn is_unique(&self, meta: &Metadata) -> bool {
-        if !self.enabled {
-            return true;
-        }
-        self.seen.lock().unwrap().insert(meta.ino())
     }
 }
 
@@ -88,5 +80,46 @@ impl Matcher for globset::GlobMatcher {
     #[inline(always)]
     fn is_file_name_match(&self, path: &Path) -> Option<bool> {
         path.file_name().map(|file_name| self.is_match(file_name))
+    }
+}
+
+#[cfg(unix)]
+mod inode {
+    use std::collections::HashSet;
+    use std::fs::Metadata;
+    use std::os::unix::fs::MetadataExt;
+    use std::sync::Mutex;
+
+    /// Filter out unique inodes
+    #[derive(Debug)]
+    pub enum Filter {
+        Disabled,
+        Enabled(InodeSet),
+    }
+
+    #[derive(Debug, Default)]
+    pub struct InodeSet(Mutex<HashSet<u64>>);
+
+    impl Filter {
+        pub fn new(disable_hard_links_filter: bool) -> Self {
+            if disable_hard_links_filter {
+                Filter::Disabled
+            } else {
+                Filter::Enabled(Default::default())
+            }
+        }
+
+        pub fn is_unique(&self, meta: &Metadata) -> bool {
+            match self {
+                Self::Disabled => true,
+                Self::Enabled(set) => set.is_unique(meta),
+            }
+        }
+    }
+
+    impl InodeSet {
+        fn is_unique(&self, meta: &Metadata) -> bool {
+            self.0.lock().unwrap().insert(meta.ino())
+        }
     }
 }
